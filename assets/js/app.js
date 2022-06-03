3c7fba0f87cbf859e27d374d6731beb663396be5
sajjadzh <sajjadzh@outlook.com>
2022-06-03 09:40:12 +0200

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,9 +27,6 @@ let header = $(`
     <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="publications.html">Publications</a></li>
-   <li class="nav-item">
-   <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
-   </li>
    <div class="bike">
    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-80 0 650 400" preserveAspectRatio="xMinYMin meet">
      <rect/>
